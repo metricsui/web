ee6b6731467aaf434b1fc5b66dfbc4e4d84275a0
Albertus Angga <albertusangga.98@gmail.com>
2020-10-06 03:05:05 +0700

    Implement simple dark mode toggler

---
+++ src/app/ThemeToggler.svelte
@@ -0,0 +1,21 @@
+<script>
+  let isDarkTheme = false
+
+  function toggleTheme() {
+    isDarkTheme = !isDarkTheme
+    window.document.body.classList.toggle('dark')
+  }
+</script>
+
+<style>
+  button {
+    margin-top: 1rem;
+    width: 10rem;
+    height: 3rem;
+    border-radius: 1000px;
+  }
+</style>
+
+<button on:click={toggleTheme}>
+  {#if isDarkTheme}Go light{:else}Go dark{/if}
+</button>
