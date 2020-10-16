3cc09bb10306e94d5f59284957c07bf08dd70c22
Albertus Angga <albertusangga.98@gmail.com>
2020-10-16 22:59:26 +0700

    Fix responsiveness

--- src/app/landing/Landing.svelte
+++ src/app/landing/Landing.svelte
@@ -1,13 +1,12 @@
 <script>
-  import { externalLinks } from '../constants/externalLinks'
-  import ThemeToggler from '../ThemeToggler.svelte'
-  import MetricsLogo from './components/MetricsLogo.svelte'
+  import Content from './components/Content.svelte'
+  import StickyNavbar from './components/StickyNavbar.svelte'
+  import Footer from './components/Footer.svelte'
 </script>
 
 <style>
   .wrapper {
     text-align: left;
-    margin-left: 12%;
     width: 100%;
     min-height: 100vh;
     display: flex;
@@ -15,81 +14,10 @@
     justify-content: center;
     align-items: flex-start;
   }
-
-  .section {
-  }
-
-  .section > * {
-    max-width: 50%;
-  }
-
-  a {
-    justify-self: flex-start;
-  }
-
-  @media screen and (max-width: 640px) {
-    .section {
-      margin-left: 5%;
-      width: 100%;
-      min-height: 100vh;
-      display: flex;
-      flex-direction: column;
-      justify-content: center;
-      align-items: flex-start;
-    }
-    .section > * {
-      max-width: 90%;
-    }
-
-    .wrapper {
-      margin-left: 5%;
-    }
-  }
-
-  .body-text {
-    line-height: 32px;
-    letter-spacing: 0.15px;
-  }
-
-  .want-to-be-notified-text {
-    margin-bottom: 16px;
-  }
-
-  h1 {
-    text-align: left;
-    line-height: 114%;
-    letter-spacing: -1.5px;
-  }
-
-  h5 {
-    margin-bottom: 1rem;
-  }
-
-  .body-text {
-    font-size: 1.25rem;
-    margin-top: 0.5rem;
-  }
 </style>
 
 <div class="wrapper">
-  <ThemeToggler />
-  <div class="section">
-    <MetricsLogo />
-    <h1 class="font-light">Coming soon to start-up your career</h1>
-  </div>
-  <div class="section">
-    <h5>What is Metrics?</h5>
-    <div class="body-text">
-      Metrics is mentoring initiative by Computer Science Alumni of Universitas
-      Indonesia that strives to help CS students start-up their career through
-      an extensive and collaborative four-months mentorship program.
-    </div>
-    <h6 class="want-to-be-notified-text">
-      Want to be notified when the magic happens?
-    </h6>
-    <div class="button-container">
-      <a href={externalLinks.earlyBirdFormURL} class="primary-button">Let me
-        know</a>
-    </div>
-  </div>
+  <StickyNavbar />
+  <Content />
+  <Footer />
 </div>
---
+++ src/app/landing/components/StickyNavbar.svelte
@@ -0,0 +1,33 @@
+<script>
+  import ThemeToggler from '../../ThemeToggler.svelte'
+</script>
+
+<style>
+  .container {
+    width: 100%;
+    z-index: 10;
+    justify-content: space-between;
+    display: flex;
+    position: fixed;
+    align-items: center;
+    top: 0;
+    left: 0;
+
+    transition: 1s;
+    -webkit-transition: 1s;
+
+    -webkit-transform: translateY(0%);
+    -ms-transform: translateY(0%);
+    transform: translateY(0%);
+  }
+
+  .container-slide-up {
+    transform: translateY(-100%);
+    -webkit-transform: translateY(-100%);
+    -ms-transform: translateX(-100%);
+  }
+</style>
+
+<div class="container">
+  <ThemeToggler />
+</div>
