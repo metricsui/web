a2fc9aaf6e948d106d74aff2c01582bd47ad5772
Albertus Angga <albertusangga.98@gmail.com>
2020-10-16 21:39:58 +0700

    Implement coming soon partially

---
+++ src/app/landing/components/MetricsLogo.svelte
@@ -0,0 +1,24 @@
+<style>
+  .metrics-logo-container {
+    position: relative;
+  }
+
+  :global(body.dark) .light-theme-logo {
+    display: none;
+  }
+
+  :global(body:not(.dark)) .dark-theme-logo {
+    display: none;
+  }
+</style>
+
+<div class="metrics-logo-container">
+  <img
+    src="images/metrics-logo-dark-text.png"
+    alt="metrics-logo-light-text.png"
+    class="light-theme-logo" />
+  <img
+    src="images/metrics-logo-light-text.png"
+    alt="metrics-logo-dark-text.png"
+    class="dark-theme-logo" />
+</div>
