2422cd4cd6f20181fca1a27093310a4695eef3da
Wisnu Ramadhan <wisnu.ramadhan@shopee.com>
2020-12-05 18:07:46 +0800

    ! navbar mobile and remove scroll sticky navbar

--- src/app/components/StickyNavbar.svelte
+++ src/app/components/StickyNavbar.svelte
@@ -1,25 +1,25 @@
 <script>
-  import { fade } from 'svelte/transition'
+  import { fade, slide } from 'svelte/transition'
   import ThemeToggler from '../ThemeToggler.svelte'
   import { handleLogin, handleLogout } from '../utils'
-  import { isLoggedIn } from '../stores'
+  import { isLoggedIn, isMobileScreen } from '../stores'
   import MetricsLogo from './MetricsLogo.svelte'
 
   export let showLogo = true
   export let useAnimation = false
+
+  let isShowingDrawer = false
+
+  function toggleDrawer() {
+    isShowingDrawer = !isShowingDrawer
+  }
 </script>
 
 <style>
   #sticky-navbar {
-    box-sizing: border-box;
-    padding-left: 5%;
-    padding-right: 5%;
     width: 100%;
     z-index: 10;
-    justify-content: flex-end;
-    display: flex;
     position: fixed;
-    align-items: center;
     top: 0;
 
     transition: 0.5s;
@@ -28,14 +28,49 @@
     -webkit-transform: translateY(0%);
     -ms-transform: translateY(0%);
     transform: translateY(0%);
-
-    margin-top: 1rem;
   }
 
-  :global(#sticky-navbar.container-slide-up) {
-    transform: translateY(-5rem);
-    -webkit-transform: translateY(-5rem);
-    -ms-transform: translateX(-5rem);
+  .drawer {
+    position: relative;
+    background-color: rgba(33, 33, 33, 0.4);
+  }
+
+  .drawer-open {
+    height: 100vh;
+  }
+
+  .nav-content {
+    background-color: var(--bg-color);
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    box-sizing: border-box;
+    padding-left: 5%;
+    padding-right: 5%;
+    width: 100%;
+    justify-content: flex-end;
+    display: flex;
+    align-items: center;
+    z-index: 11;
+  }
+
+  .drawer-content {
+    background-color: var(--bg-color);
+    padding: 1rem 2rem;
+    z-index: 10;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .drawer-content .toggler-wrapper-mobile {
+    align-self: flex-end;
+  }
+
+  .drawer-item-space {
+    height: 1.125rem;
+  }
+
+  .drawer-content button {
+    width: 100%;
   }
 
   .toggler-wrapper {
@@ -52,23 +87,57 @@
 </style>
 
 <div class="container" id="sticky-navbar">
-  {#if showLogo && useAnimation}
-    <div class="logo-wrapper" out:fade>
-      <MetricsLogo />
+  <div class={`drawer ${isShowingDrawer ? 'drawer-open' : ''}`}>
+    <div class="nav-content">
+      {#if (isShowingDrawer || showLogo) && useAnimation}
+        <div class="logo-wrapper" transition:fade>
+          <MetricsLogo />
+        </div>
+      {/if}
+      {#if (isShowingDrawer || showLogo) && !useAnimation}
+        <div class="logo-wrapper">
+          <MetricsLogo />
+        </div>
+      {/if}
+
+      <div class="space" />
+
+      {#if !$isMobileScreen}
+        <div class="toggler-wrapper">
+          <ThemeToggler />
+        </div>
+        {#if !$isLoggedIn}
+          <button class="secondary-button" on:click={handleLogin}>Login (SSO UI)</button>
+        {:else}
+          <button
+            class="secondary-button"
+            on:click={handleLogout}>Logout</button>
+        {/if}
+      {:else if isShowingDrawer}
+        <div on:click={toggleDrawer}>
+          <img src="images/ic_close.svg" alt="Close menu" in:fade />
+        </div>
+      {:else}
+        <div on:click={toggleDrawer}>
+          <img src="images/ic_hamburger_menu.svg" alt="Open menu" in:fade />
+        </div>
+      {/if}
     </div>
-  {/if}
-  {#if showLogo && !useAnimation}
-    <div class="logo-wrapper">
-      <MetricsLogo />
-    </div>
-  {/if}
-  <div class="space" />
-  <div class="toggler-wrapper">
-    <ThemeToggler />
+
+    {#if isShowingDrawer}
+      <div class="drawer-content" transition:slide>
+        {#if !$isLoggedIn}
+          <button class="secondary-button" on:click={handleLogin}>Login (SSO UI)</button>
+        {:else}
+          <button
+            class="secondary-button"
+            on:click={handleLogout}>Logout</button>
+        {/if}
+        <div class="drawer-item-space" />
+        <div class="toggler-wrapper-mobile">
+          <ThemeToggler />
+        </div>
+      </div>
+    {/if}
   </div>
-  {#if !$isLoggedIn}
-    <button class="secondary-button" on:click={handleLogin}>Login (SSO UI)</button>
-  {:else}
-    <button class="secondary-button" on:click={handleLogout}>Logout</button>
-  {/if}
 </div>
--- src/app/landing/state/scroll.js
+++ src/app/landing/state/scroll.js
@@ -2,24 +2,14 @@ export let isScrollingDown = false
 import { debounce } from '../../utils'
 import { writable } from 'svelte/store'
 
-export let lastScrollPosition = 0
 export const shouldShowNavbarLogo = writable(false)
 
 export const scrollSubscriptionHandler = debounce(
   () => {
     const wrapperDom = document.getElementById('landing-wrapper')
     const offset = wrapperDom.pageYOffset || wrapperDom.scrollTop
-    const scrollDown = offset > lastScrollPosition
 
-    if (isScrollingDown != scrollDown && offset !== 0) {
-      isScrollingDown = scrollDown
-      document
-        .getElementById('sticky-navbar')
-        .classList.toggle('container-slide-up')
-    }
-    lastScrollPosition = offset
-
-    if (lastScrollPosition < 200) {
+    if (offset < 200) {
       shouldShowNavbarLogo.set(false)
     } else {
       shouldShowNavbarLogo.set(true)
