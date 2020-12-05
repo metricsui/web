04508ec7adae1a777429d1d40c0df3ad382461a1
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2020-12-05 20:05:07 +0800

    Update api.js

--- src/app/api.js
+++ src/app/api.js
@@ -3,5 +3,5 @@ import { post } from './http'
 const DOMAIN = window.location.origin
 
 export function postAuthTicket(ticket) {
-  return post('auth/', { ticket, service: DOMAIN + '/' })
+  return post('auth/', { ticket, service: DOMAIN })
 }
