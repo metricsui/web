c611eab5e46e776e7fd9f5e641d5492f55b4e6cc
Wisnu Ramadhan <wisnu.ramadhan@shopee.com>
2020-12-05 15:30:06 +0800

    debounce scroll event

--- src/app/landing/state/scroll.js
+++ src/app/landing/state/scroll.js
@@ -1,15 +1,22 @@
 export let isScrollingDown = false
+import { debounce } from '../../utils'
 export let lastScrollPosition = 0
 
-export function scrollSubscriptionHandler() {
-  const wrapperDom = document.getElementById('landing-wrapper')
-  const offset = wrapperDom.pageYOffset || wrapperDom.scrollTop
-  const scrollDown = offset > lastScrollPosition
-  if (isScrollingDown != scrollDown && offset !== 0) {
-    isScrollingDown = scrollDown
-    document
-      .getElementById('sticky-navbar')
-      .classList.toggle('container-slide-up')
-  }
-  lastScrollPosition = offset
-}
+export const scrollSubscriptionHandler = debounce(
+  () => {
+    const wrapperDom = document.getElementById('landing-wrapper')
+    const offset = wrapperDom.pageYOffset || wrapperDom.scrollTop
+    const scrollDown = offset > lastScrollPosition
+
+    console.log(wrapperDom.scrollTop, wrapperDom.pageYOffset)
+    if (isScrollingDown != scrollDown && offset !== 0) {
+      isScrollingDown = scrollDown
+      document
+        .getElementById('sticky-navbar')
+        .classList.toggle('container-slide-up')
+    }
+    lastScrollPosition = offset
+  },
+  25,
+  false
+)
--- src/app/utils.js
+++ src/app/utils.js
@@ -99,3 +99,19 @@ export function addColorSchemeEventListener(callback) {
     },
   }
 }
+
+export function debounce(func, wait, immediate) {
+  var timeout
+  return function () {
+    var context = this,
+      args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
