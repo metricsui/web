a72017215e8c5781a3e019d4d68bfb7579a0ea7f
Wisnu Ramadhan <wisnu.ramadhan@shopee.com>
2020-12-07 00:05:24 +0800

    rmove unused css

--- src/app/landing/components/how-we-are-solving-problem/HowWeAreSolvingProblem.svelte
+++ src/app/landing/components/how-we-are-solving-problem/HowWeAreSolvingProblem.svelte
@@ -7,48 +7,6 @@
     padding: var(--landing-section-padding-vertical) var(--horizontal-margin);
   }
 
-  .activity {
-    display: flex;
-  }
-
-  .activity .img-wrapper {
-    width: 296px;
-    height: 154px;
-    position: relative;
-    background-color: lightgray;
-  }
-
-  .activity img {
-    object-fit: cover;
-    width: 100%;
-    height: 100%;
-  }
-
-  .activity .img-wrapper::before {
-    content: '';
-    position: absolute;
-    background: linear-gradient(
-      180deg,
-      rgba(216, 49, 91, 0.7) 0%,
-      rgba(216, 49, 91, 0.5) 100%
-    );
-    background-blend-mode: multiply;
-    width: 100%;
-    height: 100%;
-  }
-
-  .activity .title {
-    font-size: var(--h6-size);
-    line-height: 2.25rem;
-  }
-
-  .activity .description {
-    font-size: var(--h6-size);
-    width: 60%;
-    margin-top: 0.5rem;
-    line-height: 2.25rem;
-  }
-
   @media only screen and (min-width: 1024px) {
     .section h3 {
       width: 50%;
@@ -56,27 +14,10 @@
   }
 
   @media only screen and (max-width: 640px) {
-    .activity {
-      flex-direction: column;
-    }
-    .activity .img-wrapper {
-      width: 100%;
-      background-color: lightgray;
-    }
-
-    .activity .description {
-      width: 100%;
-    }
-
     .space-activity {
       height: 2.5rem;
       width: 2.5rem;
     }
-  }
-
-  .space {
-    height: 1rem;
-    width: 2.5rem;
   }
 
   .space-activity {
